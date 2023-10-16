f8b1431c069e64ba980c39a8ba77c7269441bafa
Fernando Rodrigues <Fernando.hardd@hotmail.com>
2023-10-16 16:19:20 -0300

    useState e event on input

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,13 @@
+import { useState } from 'react'
+
 export const App = () => {
+  const [search, setSearch] = useState('') // estado da pesquisa setado sem valor inicial
+  console.log(search)
   return (
     <>
       <div className="box-app">
         <h4>League of Legends App</h4>
-        <input type="text"></input>
+        <input type="text" onChange={(e) => setSearch(e.target.value)}></input>
         <button>Search</button>
       </div>
     </>
