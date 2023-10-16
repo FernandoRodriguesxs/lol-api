7495a7b8c739012356d86ed35b9bb0f5774fdfc6
Fernando Rodrigues <Fernando.hardd@hotmail.com>
2023-10-16 16:04:29 -0300

    clear

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+export const App = () => {
+  return (
+    <>
+      <div className="box-app">
+        <h4>League of Legends App</h4>
+        <input type="text"></input>
+        <button>Search</button>
+      </div>
+    </>
+  )
+}
